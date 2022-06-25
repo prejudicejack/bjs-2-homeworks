f0b971de84018422efbb591975d040b1f7290edf
prejudicejack <SavinaVG@yandex.ru>
2022-06-25 20:14:05 +0700

    added resolved tasks

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,13 @@
 function compareArrays(arr1, arr2) {
   let result;
-
-  // Ваш код
-
+  result = (arr1.length === arr2.length && arr1.every((value, index) => value === arr2[index]));
+  
   return result; // boolean
 }
 
 function advancedFilter(arr) {
   let resultArr;
-
-  // Ваш код
+  resultArr = arr.filter((number) => number % 3 === 0 && number > 0).map((number) => number * 10);
 
   return resultArr; // array
 }
