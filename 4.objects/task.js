810d51c7c7440e7a42ebd90acb133c96e0931f1d
prejudicejack <SavinaVG@yandex.ru>
2022-06-29 22:46:55 +0700

    working prototype added

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,41 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
 }
+
+let student1 = new Student();
+let student2 = new Student();
+let student3 = new Student();
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+    if(this.marks === undefined) { 
+      this.marks = [];
+      this.marks.push(mark);
+    } else {
+      this.marks.push(mark);
+    } 
+}
+
+Student.prototype.addMarks = function (mark1,mark2,mark3,...rest) {
+    if(this.marks === undefined) { 
+      this.marks = [];
+      this.marks.push(mark1,mark2,mark3,...rest);
+    } else {
+      this.marks.push(mark1,mark2,mark3,...rest);
+    } 
+}
+
+Student.prototype.getAverage = function () {
+  return this.marks.reduce((a, b) => a + b, 0) / this.marks.length
+}
+
+Student.prototype.exclude = function exclude(reason) {
+  this.excluded = reason;
+  delete this.subject;
+  delete this.marks;
+}
